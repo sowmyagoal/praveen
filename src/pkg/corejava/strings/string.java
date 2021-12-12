445d2d4d44f877890d09c0a953a7dd2be670017d
Sowmya <Sowmya@DESKTOP-U6M3JIM>
2021-12-12 21:58:46 +0530

    String concept

---
+++ src/pkg/corejava/strings/string.java
@@ -0,0 +1,15 @@
+package pkg.corejava.strings;
+
+public class string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubs
+		String s1 = "Dream job";
+		String s2 = "Dream job";
+		String s3 = "focus";
+		
+		System.out.println(s1.charAt(4));
+		System.out.println(s1.compareTo(s2));
+	}
+
+}
